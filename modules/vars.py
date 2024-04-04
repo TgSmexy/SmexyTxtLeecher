b89e92ec657b774c1c4b9238289718866f3df4fc
telegrambots0 <146921432+telegrambots0@users.noreply.github.com>
2024-04-04 14:22:02 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,7 @@
-api_id = 28912814
-api_hash = "8c69efa7804f6f3eb167984e2f4cf549"
-bot_token = "6647623608:AAEam-VeKvC4I0mbaaXwym15j0hw3l0Wtes"
-# Bot Username = "@jaishriramtxtBot"
+#𝐌𝐚𝐝𝐞 𝐁𝐲  : [𝙎𝙈𝙀𝙓𝙔 𝙎𝙏𝙊𝙍𝙀 🇮🇳](https://t.me/SmexyStore)
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+
+api_id = "23989445"
+api_hash = "f9d8341dc5ad6978e623f34df457b6ed"
+bot_token = "6700172868:AAFf-yHdF9JHEmVW9yNBKRkzeik1X2ijZok"
+# Bot Username = "https://t.me/my_mikku_bot"
