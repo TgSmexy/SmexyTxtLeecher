986ca895cb5f6979ee333103b32d7a3f37eb65f8
telegrambots0 <146921432+telegrambots0@users.noreply.github.com>
2024-04-12 18:10:44 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,5 +3,5 @@
 
 api_id = "23989445"
 api_hash = "f9d8341dc5ad6978e623f34df457b6ed"
-bot_token = "6700172868:AAFf-yHdF9JHEmVW9yNBKRkzeik1X2ijZok"
-# Bot Username = "https://t.me/my_mikku_bot"
+bot_token = "7078360884:AAEfuTn8za8py7_7Cxy2zlKNReAD3zVzIV8"
+# Bot Username = "https://t.me/SmexyTxtLeecherBot"
