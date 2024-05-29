86e53cf729280b5d4318544ba2ba67870e4845a0
telegrambots0 <146921432+telegrambots0@users.noreply.github.com>
2024-05-29 19:16:07 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,7 +1,7 @@
 #𝐌𝐚𝐝𝐞 𝐁𝐲  : [𝙎𝙢𝙚𝙭𝙮 ᥫ᭡](https://t.me/SmexyOP)
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 
-api_id = "28619451"
-api_hash = "e41e306cd0e0d0be11ec78c281f0b628"
-bot_token = "7084783725:AAFqfIvmE25YPJ_rliHaNES6a5Xxmsq3Vsc"
+api_id = "25263708"
+api_hash = "9bde48b267ce65a576b478c0ff8c7bbb"
+bot_token = "7395475299:AAF4UK-zkoanFNX2s0p4UdsGXIQF5POr8gU"
 # Bot Username = "https://t.me/SmexyTxtLeechingBot"
